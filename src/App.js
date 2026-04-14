d1edc62b921d652993c9f942b7f7f60980791793
kishore <kishoreram10106@gmail.com>
2026-04-14 16:28:58 +0530

    to test dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div>
       <Register />
       <Login />
+      <Dashboard/>
     </div>
   );
 }
