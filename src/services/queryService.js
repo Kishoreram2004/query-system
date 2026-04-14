1d8cb070f909a1aa0b8987c4ac65be4cc8176d5d
kishore <kishoreram10106@gmail.com>
2026-04-14 16:37:52 +0530

    Add Comment Service

--- src/services/queryService.js
+++ src/services/queryService.js
@@ -1,10 +1,13 @@
 import { db } from "../firebase/config";
+//import { collection, addDoc, query, where, onSnapshot } from "firebase/firestore";
 import {
   addDoc,
   collection,
+  where,
   onSnapshot,
   updateDoc,
-  doc
+  doc,
+  query
 } from "firebase/firestore";
 
 // Add Query
@@ -26,4 +29,26 @@ export const getQueries = (callback) => {
 // Update status
 export const updateQueryStatus = (id, status) => {
   return updateDoc(doc(db, "queries", id), { status });
+};
+
+
+// Add comment
+export const addComment = async (comment) => {
+  await addDoc(collection(db, "comments"), comment);
+};
+
+// Get comments for a query
+export const getComments = (queryId, callback) => {
+  const q = query(
+    collection(db, "comments"),
+    where("queryId", "==", queryId)
+  );
+
+  return onSnapshot(q, (snapshot) => {
+    const comments = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+    callback(comments);
+  });
 };
